57a86b2ec88454cdedd789db6bfe7ba899e30ed6
Ростислав Катаргин <katargin53@gmail.com>
2026-01-08 21:03:47 +0500

    filled cabinet/me with data in a card, added a field in user.interface to hold surname

--- app/pages/cabinet/me.vue
+++ app/pages/cabinet/me.vue
@@ -1,14 +1,51 @@
 <script setup lang="ts">
 definePageMeta({ middleware: 'auth' })
 const auth = useAuth();
+let {user} = auth 
 
 </script>
 
 <template>
-  <v-btn>
-    Тест
-  </v-btn>
-  <v-btn @click="auth.logout()">
-    Выйти
-  </v-btn>
+  <v-container class="d-flex align-center justify-center" style="min-height: 80vh;">
+    <v-card class="pa-6" width="400" elevation="4">
+      <v-card-title class="text-center text-h5 mb-4">
+        Личный кабинет
+      </v-card-title>
+      <v-card-text class="text-center">
+        <div class="mb-4">
+          <div class="text-subtitle-2 text-grey">Имя и фамилия</div>
+          <div class="text-h6">{{ user?.name }} {{ user?.surname }}</div>
+        </div>
+        <div class="mb-6">
+          <div class="text-subtitle-2 text-grey">Email</div>
+          <div class="text-body-1">{{ user?.email }}</div>
+        </div>
+        
+        <!-- Additional user info blocks can be added here -->
+        <!-- Example: Phone number block -->
+        <!-- <div class="mb-4">
+          <div class="text-subtitle-2 text-grey">Телефон</div>
+          <div class="text-body-1">{{ user?.phone || 'Не указан' }}</div>
+        </div> -->
+      </v-card-text>
+      <v-card-actions class="d-flex justify-space-between mt-2">
+        <v-btn 
+          color="primary" 
+          variant="outlined"
+          @click=""
+          class="flex-grow-1 mr-2"
+        >
+          Изменить данные
+        </v-btn>
+        <v-btn 
+          color="error" 
+          variant="outlined"
+          @click="auth.logout()"
+          class="flex-grow-1 ml-2"
+        >
+          Выйти
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-container>
 </template>
