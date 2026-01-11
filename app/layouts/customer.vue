f30ab5ec9b7d054f16f22aa0fd2092922895a168
MrMefedron <gleb70706@gmail.com>
2026-01-11 12:59:02 +0500

    delete cart from v-menu

--- app/layouts/customer.vue
+++ app/layouts/customer.vue
@@ -48,7 +48,6 @@ async function logOut() {
             <v-card min-width="200">
               <v-list>
                 <v-list-item @click="router.push('/cabinet/me')" prepend-icon="mdi-account-edit" title="Личный кабинет" />
-                <v-list-item @click="router.push('/cabinet/cart')" prepend-icon="mdi-cart" title="Корзина" />
                 <v-divider class="my-2"></v-divider>
                 <v-list-item @click="logOut" prepend-icon="mdi-logout" title="Выйти" />
               </v-list>
